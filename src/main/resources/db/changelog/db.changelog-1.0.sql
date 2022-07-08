37b3acf55e40e7c17cf65c83803509474d028af4
EgorZhuganov <egor-zhuganov@yandex.ru>
2022-07-08 18:55:03 +1000

    fix: изменить db.changelog-1.0.sql

--- src/main/resources/db/changelog/db.changelog-1.0.sql
+++ src/main/resources/db/changelog/db.changelog-1.0.sql
@@ -1,6 +1,9 @@
 --liquibase formatted sql
 
 --changeset zhuganov:1
+create type status as enum ('WAITING', 'APPROVED', 'REJECTED', 'CANCELED');
+
+--changeset zhuganov:2
 create table if not exists users
 (
     id    bigserial primary key,
@@ -8,7 +11,7 @@ create table if not exists users
     email varchar(512) unique not null
 );
 
---changeset zhuganov:2
+--changeset zhuganov:3
 create table if not exists item_request
 (
     id           bigserial primary key,
@@ -17,7 +20,7 @@ create table if not exists item_request
     requester_id bigint references users (id)
 );
 
---changeset zhuganov:3
+--changeset zhuganov:4
 create table if not exists item
 (
     id          bigserial primary key,
@@ -25,31 +28,30 @@ create table if not exists item
     description varchar(4000),
     available   boolean,
     owner_id    bigint not null references users (id),
-    request_id  bigint references item_requests (id),
+    request_id  bigint references item_request (id),
     UNIQUE (owner_id, name)
 );
 
---changeset zhuganov:4
+--changeset zhuganov:5
 create table if not exists booking
 (
     id              bigserial primary key,
     start_date_time timestamp without time zone not null,
     end_date_time   timestamp without time zone not null,
-    approved        boolean                     not null,
-    canceled        boolean                     not null,
-    item_id         bigint references items (id),
+    --approved        boolean                     not null, --вариант практикума
+    --canceled        boolean                     not null, --вариант практикума
+    status          status                      not null,
+    item_id         bigint references item (id),
     booker_id       bigint references users (id)
 );
 
---changeset zhuganov:5
+--changeset zhuganov:6
 create table if not exists comment
 (
     id        bigserial primary key,
     text      varchar(4000)               not null,
     created   timestamp without time zone not null,
-    item_id   bigint references items (id),
+    item_id   bigint references item (id),
     author_id bigint references users (id)
 );
 
---changeset zhuganov:6
-create type status as enum ('WAITING', 'APPROVED', 'REJECTED', 'CANCELED');
